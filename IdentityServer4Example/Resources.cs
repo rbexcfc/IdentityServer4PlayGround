bc843c72f95a9fcfb72e469751a897f43181fe25
rbexcfc <richie.bexhell@zupatech.co.uk>
2018-01-09 16:11:15 +0000

    Initial commit

---
+++ IdentityServer4Example/Resources.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServer4Example
+{
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GeIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string>{"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "customAPI",
+                    DisplayName = "Custom API",
+                    Description = "Custom API Access",
+                    UserClaims = new List<string>{"role"},
+                    ApiSecrets = new List<Secret>{new Secret("superSecret".Sha256())},
+                    Scopes = new List<Scope>
+                    {
+                        new Scope("customAPI.read"),
+                        new Scope("customAPI.write")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer4Example/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.Test;
+
+namespace IdentityServer4Example
+{
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Rich",
+                    Password = "password",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.Email,"notsolite@hotmail.co.uk"),
+                        new Claim(JwtClaimTypes.Role,"admin")
+                    }
+                }
+            };
+        }
+    }
+}
